a51feca3fb38db6a4e613c557dd16c96afeceabf
eriksawchuk96 <erik.sawchuk@anima.edu.uy>
2026-05-29 22:20:13 -0300

    Clase 29.05

--- mi-servidor/index.js
+++ mi-servidor/index.js
@@ -19,6 +19,26 @@ app.post("/productos", (req, res) => {
 app.delete("/productos", (req, res) => {
   res.send("Producto eliminado");
 });
+app.get("/articulos/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Artículo con id: ${id}`);
+});
+app.get("/usuarios/:userId/posts/:postId", (req, res) => {
+  const { userId, postId } = req.params;
+  res.json({ userId, postId });
+});
+app.get("/articulos", (req, res) => {
+  const categoria = req.query.categoria;
+  const orden = req.query.orden;
+  res.json({ categoria, orden });
+});
+app.get("/buscar", (req, res) => {
+  const queryBusqueda = req.query.q;
+  if (!queryBusqueda) {
+    return res.send("Ingresá un término de búsqueda.");
+  }
+  res.send(`Buscaste: ${queryBusqueda}`);
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
