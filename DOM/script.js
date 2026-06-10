d6cbec5960e4b4a988b387f779ca04e94becef1c
eriksawchuk96 <erik.sawchuk@anima.edu.uy>
2026-06-10 20:10:55 -0300

    Clase 10.06

--- DOM/script.js
+++ DOM/script.js
@@ -12,3 +12,25 @@ const parrafo = document.getElementById("resultado");
 console.log("Contenido original:", parrafo.textContent);
 parrafo.textContent = "Texto modificado con JS";
 parrafo.innerHTML = "<strong>Texto modificado</strong>";
+const titulo = document.querySelector("h1");
+titulo.style.color = "darkblue";
+const primerParrafo1 = document.querySelector("p");
+primerParrafo1.classList.add("resaltado");
+console.log(primerParrafo1);
+let tieneClase = primerParrafo1.classList.contains("resaltado");
+console.log("¿El párrafo tiene la clase .resaltado?:", tieneClase);
+primerParrafo1.classList.remove("resaltado");
+tieneClase = primerParrafo1.classList.contains("resaltado");
+console.log("¿El párrafo todavía tiene la clase?:", tieneClase);
+
+const peliculas = [
+  { titulo: "Toy Story", anio: 1995 },
+  { titulo: "Parasite", anio: 2019 },
+  { titulo: "El Padrino", anio: 1972 },
+  { titulo: "Interstellar", anio: 2014 },
+];
+const listaPeliculas = document.getElementById("lista-peliculas");
+peliculas.forEach((pelicula) => {
+  const itemHTML = `<li>${pelicula.titulo} (${pelicula.anio})</li>`;
+  listaPeliculas.insertAdjacentHTML("beforeend", itemHTML);
+});
